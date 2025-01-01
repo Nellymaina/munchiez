26295e03d30b411c40871e2f394894fa70acf5ae
Nellymaina <nellymaina095@gmail.com>
2025-01-01 20:30:11 +0300

    new

--- src/components/Cartpage.js
+++ src/components/Cartpage.js
@@ -7,6 +7,7 @@ import {DELETEALL} from './deleteall-action'
 import {Link} from 'react-router-dom'
 
 export default function Cartpage(){
+const totalItems=useSelector(state=>state.totalItems)
 const prod=useSelector(state=>state.items)
 const dispatch=useDispatch();
 
@@ -30,20 +31,10 @@ const sum=checksum()
         dispatch({type:DELETEALL, payload:item})
              
              }
- 
-
-    return(
-
-
-        <div className='cart-page'>
-            <h2 className='hero-title'>What's in Your Cart</h2>
-
-            {prod.map(item=>{
-   return(
-    
-
+ const cart=prod.map(item=>{
+            return(
+   
 < div className="cart-div">
-
 <div className='cart-image'>
 <img src={item.image} alt="" className="cartItems" />
 <p className="cart-name">{item.FullName}</p>
@@ -51,11 +42,19 @@ const sum=checksum()
 <div className='cart-des'>
 
 <ul className='cart-ul'>price<li className="check-span">KSH {item.price}.00</li></ul>
-    <button className="remove" onClick={()=>handleCartRemove(item)}>-</button><p className="cart-text">{item.quantity}</p><button className="add" onClick={()=>handleCart(item)}>+</button>
-<ul className='cart-ul'>Total<li className="check-span">KSH {item.price*item.quantity}.00</li></ul>
+   <ul className='cart-ul'>Quantity<div className='cart-quantity'><li className="remove" onClick={()=>handleCartRemove(item)}>-</li><li className="cart-text">{item.quantity}</li><li className="add" onClick={()=>handleCart(item)}>+</li></div></ul>
+   <ul className='cart-ul'>Total<li className="check-span">KSH {item.price*item.quantity}.00</li></ul>
 </div>
 </div>
-   )})}
+   )})
+
+    return(
+
+
+        <div className='cart-page'>
+            <h2 className='hero-title'>What's in Your Cart?</h2>
+
+           {cart}
 
    <div className="cart-sum">SUBTOTAL KSH {sum}.00</div>
    <div className='checkout-div'>
